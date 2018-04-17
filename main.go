0ae21f3cd4659670e300ba76d8d7958936945518
Junichi Sato <sat0b.dev@gmail.com>
2018-04-17 23:30:08 +0900

    add save and load function

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
+
+func showUsage() {
+	fmt.Println("Usage: kensaku [xmlfile]")
+}
 
 type MediaWiki struct {
 	Page []Page `xml:"page"`
@@ -22,17 +27,7 @@ type Revision struct {
 	Text string `xml:"text"`
 }
 
-func showUsage() {
-	fmt.Println("Usage: kensaku [xmlfile]")
-}
-
-func main() {
-	if len(os.Args) != 2 {
-		showUsage()
-		return
-	}
-	fileName := os.Args[1]
-
+func loadXml(fileName string) *MediaWiki {
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal("error: %v", err)
@@ -50,5 +45,68 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", mediaWiki)
+	return mediaWiki
+}
+
+func convertDocument(mediaWiki *MediaWiki) []Document {
+	var documents []Document
+	for i, page := range mediaWiki.Page {
+		var document Document
+		document.Title = page.Title
+		document.Text = page.Revision.Text
+		document.Id = i
+		documents = append(documents, document)
+	}
+	return documents
+}
+
+type Document struct {
+	Title string
+	Text  string
+	Id    int
+}
+
+func saveIndex(fileName string) {
+	mediaWiki := loadXml(fileName)
+	documents := convertDocument(mediaWiki)
+
+	b, err := json.Marshal(documents)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Create(`/tmp/index.json`)
+	defer file.Close()
+	file.Write(b)
+}
+
+func readIndex(fileName string) []Document {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal("error: %v", err)
+	}
+
+	var documents []Document
+	json.Unmarshal(data, &documents)
+	return documents
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		showUsage()
+		return
+	}
+	fileName := os.Args[1]
+
+	saveIndex(fileName)
+	documents := readIndex("/tmp/index.json")
+
+	for i, document := range documents {
+		fmt.Println(i, document)
+	}
 }
